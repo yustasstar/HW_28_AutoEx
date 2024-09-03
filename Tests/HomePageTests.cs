4d3f61b8effdb380cba4aab30e54d7748bb136ed
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-09-03 10:49:11 +0300

    base page update

--- Tests/HomePageTests.cs
+++ Tests/HomePageTests.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics.Metrics;
 using System.Text.RegularExpressions;
 using HW_28_AutoEx.Pages;
 using Microsoft.Playwright;
+using NUnit.Framework.Internal;
 
 namespace HW_28_AutoEx.Tests
 {
@@ -52,5 +54,19 @@ namespace HW_28_AutoEx.Tests
             await _homePage.ClickPageLink("Contact us");
             await _contactPage.VerifyPageHeading("Contact Us");
         }
+
+        //Test Case 6: Contact Us Form
+
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        //3. Verify that home page is visible successfully
+        //4. Click on 'Contact Us' button
+        //5. Verify 'GET IN TOUCH' is visible
+        //6. Enter name, email, subject and message
+        //7. Upload file
+        //8. Click 'Submit' button
+        //9. Click OK button
+        //10. Verify success message 'Success! Your details have been submitted successfully.' is visible
+        //11. Click 'Home' button and verify that landed to home page successfully
     }
 }
